a70a5db28881ddda4fb6bd91839c4f819d60fe2c
Daniel Farias <danielfariias15@gmail.com>
2022-07-06 09:30:48 -0300

    Create Tag Repository, Entity, Migration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express'
-import 'reflect-metadata'
+import express, { NextFunction, Request, Response } from 'express'
 
+import 'reflect-metadata'
+import 'express-async-errors'
 import './database'
 
 import { router } from './routes'
@@ -10,5 +11,15 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({status: 'error', message: 'Internal Server Error'})
+})
 
 app.listen(3333, () => console.log('Server is running!'))
