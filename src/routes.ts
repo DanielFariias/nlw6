16f7ab57b89c5528da41f2b736ee50e56fad75b5
Daniel Farias <danielfariias15@gmail.com>
2022-07-06 23:12:16 -0300

    Create List users & tags

--- src/routes.ts
+++ src/routes.ts
@@ -9,15 +9,20 @@ import { CreateTagController } from "./controllers/CreateTagController";
 
 import { ensureAuthentication } from "./middlewares/ensureAuthenticated";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 
 const router = Router()
 
-const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
-const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController()
+const listUserSendComplimentsController = new ListUserSendComplimentsController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
-const createTagController = new CreateTagController()
 const createUserController = new CreateUserController()
+const createTagController = new CreateTagController()
+const listUsersController = new ListUsersController()
+const listTagsController = new ListTagsController()
+
 
 router.post('/tags',ensureAuthentication, ensureAdmin, createTagController.handle)
 router.post('/users', createUserController.handle)
@@ -34,5 +39,9 @@ router.get(
   ensureAuthentication,
   listUserReceiveComplimentsController.handle
 );
+
+router.get("/tags", ensureAuthentication, listTagsController.handle);
+
+router.get("/users", ensureAuthentication, listUsersController.handle);
 
 export { router }
